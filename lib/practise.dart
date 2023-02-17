70e1dcb2fbf30d280a9d98f68cdf6cdefc7e0cdf
Anchal <anchal.sri1688@gmail.com>
2023-02-17 14:55:27 +0530

    var vs dynamic

--- lib/practise.dart
+++ lib/practise.dart
@@ -1,35 +1,27 @@
 
-import 'dart:io';
 
 void main(){
 
+
   print('Welcome to Dart!');
-  stdout.write('Enter your name :');
-  var nameA = stdin.readLineSync();
-  print("Welcome, $nameA");
-  Human(); // Creating an Class object
+  String name="RAMAN";
 
-  //Declaration of variable
-    int a;
 
-  //Assign
-   a=5; //initialization
-   print(a);
-   BigInt longValue;
-   longValue = BigInt.parse('5467666746475675756575657756565754344455657565');
-   num percentage=99.6;
-   bool isLogin=false;
-   print(longValue);
+  // var
 
-  //inline declaration
-   String name ="Raman";
-   name ="Ramanujan";
+  var section;
+
+  section=7;    //int
+
+  section="D";  //String
+
+  section=false;  //bool
+
+
+  dynamic subject="Maths";
+  subject="English";
+
 
 
 }
 
-class Human{
-
-Human();
-
-}
